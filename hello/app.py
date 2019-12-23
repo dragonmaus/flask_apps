0cc0f48427f4e952ff0df805194a99849070a4bf
David Wildasin <dragonmaus@posteo.net>
2019-12-23 06:18:34 +0000

    Add favicon

--- hello/app.py
+++ hello/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, escape, redirect, request, url_for
+import os
+
+from flask import Flask, escape, redirect, request, send_from_directory, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return redirect(url_for('hello'))
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello')
 def hello():
